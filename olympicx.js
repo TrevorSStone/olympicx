a258811811dc8ec64a3d27693686bedc0a9bd167
Trevor S Stone <TrevorSStone@gmail.com>
2015-01-22 01:22:53 -0500

    Basic Functionality
    Code is pretty terrible, but can create and join teams. Can add and
    subtract points from teams in admin interface.

--- olympicx.js
+++ olympicx.js
@@ -1,33 +1,60 @@
-Tasks = new Mongo.Collection("tasks");
 Teams = new Mongo.Collection("teams");
+collapsibleCount = 0;
 if (Meteor.isClient) {
-    $(document).ready(function(){
+  $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
   // This code only runs on the client
 Template.body.helpers({
-  tasks: function () {
-
-    if (Session.get("hideCompleted")) {
-     
-      // If hide completed is checked, filter tasks
-      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-    } else {
+  teams:function () {
       // Otherwise, return all of the tasks
-      return Tasks.find({}, {sort: {createdAt: -1}});
+       if (window.location.pathname == "/admin"){
+      return Teams.find({}, {sort: {teamName: -1}});
+      
     }
+      return Teams.find({}, {sort: {score: -1}});
+
   },
   hideCompleted: function () {
     return Session.get("hideCompleted");
   },
   sessionName: function(){
+
     return Session.get("sessionName")
   },
   hideCompletedcheck: function(){
     return Session.get("hideCompleted")
+  },
+  teamID : function(){
+    return Session.get("teamID")
   }
 });
+Template.team.rendered = function(event){
+ $('.collapsible-header').unbind( "click" );
+   $('.collapsible').collapsible({ "accordion" : false });
+
+}
+
+Template.joinmodal.rendered = function(event){
+  $('.modal-trigger').leanModal();
+}
+
+Template.team.helpers({
+  myid: function (team) {
+    if(team._id ===Session.get("teamID")){
+      return "myteam";
+    }
+    return "";
+  },
+  admin: function(){
+    if (window.location.pathname == "/admin"){
+      return true
+    }
+    return false
+  }
+});
+
   Template.body.events({
   "submit .new-task": function (event) {
     // This function is called when the new task form is submitted
@@ -42,26 +69,92 @@ Template.body.helpers({
     event.target.text.value = "";
 
     // Prevent default form submit
-  Session.set("sessionName", text);
+
 
     return false;
   },
   "change .hide-completed input": function (event) {
   Session.set("hideCompleted", event.target.checked);
-}
-});
-  Template.task.events({
-  "click .toggle-checked": function () {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {$set: {checked: ! this.checked}});
+},
+"submit .new-team": function (event){
+    submitTeam(event);
+    return false;
   },
-  "click .delete": function () {
-    Tasks.remove(this._id);
+  "click .add-team-submit":submitTeam,
+  "submit .join-team": function(event){
+    addTeammate(event);
+    return false;
+  },
+  "click .join-team-submit":addTeammate,
+
+});
+
+
+Template.team.events({
+  "click .add": function () {
+    // Set the checked property to the opposite of its current value
+    Teams.update(this._id, {$inc: {score: 1}});
+  },
+  "click .sub": function () {
+    Teams.update(this._id, {$inc: {score: -1}});
+   
   }
 });
+Template.team.helpers
+
+
+Template.registerHelper("sessionName", function(){
+
+    return Session.get("sessionName")
+  });
+
 }
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function submitTeam(event) {
+  var teamname = $("#teamname").val();
+    var playername = $("#captain").val();
+    if (!teamname || !playername){
+     toast('Fill out both fields', 3000)
+      return
+    }
+    id = Teams.insert({
+      teamName: teamname,
+      player1: playername,
+      score: 0,
+      createdAt: new Date() // current time
+    });
+  Session.set("teamID", id)
+  Session.set("sessionName", teamname);
+  // Session.set("teamid",)
+  $('#modal1').closeModal();
+}
+
+function addTeammate(event){
+form = event.target;
+if (form.className != "join-team"){
+  form = form.parentElement;
+}
+
+name =$(form).find("input").val()
+   if (!name){
+     toast('Fill out both fields', 3000);
+      return false;
+    }
+    id = $(form).find("[name='id']").val();
+    teamname = $(form).find("[name='teamname']").val()
+       if (!id || !teamname){
+     toast('There was an error', 3000);
+      return false;
+    }
+
+  Session.set("teamID", id)
+  Session.set("sessionName", teamname);
+
+    Teams.update(id, {$set: {"player2": name}});
+$('#modal'+id).closeModal();
 }
